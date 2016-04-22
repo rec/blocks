b1f40d895c2ae09578c0ca94d8d2a7f5b75c70bf
Tom Ritchford <tom@swirly.com>
2016-04-22 18:12:11 +0200

    Correct permutations and apply to blocks.

--- blocks.py
+++ blocks.py
@@ -22,7 +22,6 @@ This gives us four cubes:
 
 Note that the first cube is the only one with three of the same color.
 
-
 In the frame, sides 2 and 4 are not visible.  We want to arrange the
 four cubes we have so that all four colors appear on each side 0, 1, 3, 5.
 
@@ -35,7 +34,7 @@ cube keeping the front facing us.
 We then rotate so that another side is facing us.
 We have to alternate two motions to do that:
 
-Rotating Side 0 "up" - permuting (0, 1, 3, 5)
+Rotating Side 0 "up" - permuting (0, 1, 5, 3)
 Rotating Side 0 "to the right" - permuting (0, 2, 5, 4)
 
 """
@@ -44,7 +43,7 @@ def twist(f, u, r, a, l, b):
     return f, r, a, l, u, b
 
 def up(f, u, r, a, l, b):
-    return b, f, r, u, l, a
+    return u, b, r, f, l, a
 
 def right(f, u, r, a, l, b):
     return l, u, f, a, b, r
@@ -62,3 +61,19 @@ def perms(perm=None):
 
 PERMS = perms()
 assert len(set(PERMS)) == 24
+
+for i, p in enumerate(PERMS):
+    print i, ':', p
+
+# These are the actual blocks we have:
+BLOCKS = 'bgyyyr', 'bgyrry', 'bgygbr', 'bggryr'
+
+def apply(perm, block):
+    return ''.join(block[p] for p in perm)
+
+def permute_blocks(blocks, perms):
+    return tuple(tuple(apply(p, b) for p in perms) for b in blocks)
+
+PERMUTED_BLOCKS = permute_blocks(BLOCKS, PERMS)
+
+# assert len(set(PERMUTED_BLOCKS)) == len(PERMUTED_BLOCKS)
